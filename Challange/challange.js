323ed56b9b65bf29a073b34fee528ec694b9da6e
ArtilleryTrigger <artillerytrigger@gmail.com>
2021-07-06 18:43:10 +0300

    init

---
+++ Challange/challange.js
@@ -0,0 +1,8 @@
+const prc = Number(prompt("enter price:"));
+const tax = Number(prompt("enter tax:"));
+var totalprc = 0;
+function totalPrice(prc, tax) {
+    const totalprc = prc + (prc*tax)/100;
+    return totalprc;
+}
+console.log(totalPrice(prc, tax));
