8b2b0c6ce84145fcd525cfa1540a186e9ac579ef
ArtilleryTrigger <artillerytrigger@gmail.com>
2021-07-07 21:12:10 +0300

    init

---
+++ Arrays/array.js
@@ -0,0 +1,45 @@
+// const cars = ['Audi', 'Opel', 'Fiat']
+// console.log(typeof cars);
+// console.log(cars);
+
+// const numbers = new Array(2,10)
+// console.log(numbers);
+// const numbers2 = new Array(10)
+// console.log(numbers2);
+
+// const cars = ['Audi', 'Opel', 'Fiat']
+// console.log('lenght of car :',cars.length);
+
+// const cars = ['Audi', 'Opel', 'Fiat']
+// const cars2 = ['BMW','Ferrari']
+// const cars3 = cars.concat(cars2, 'Mercedes',2021)
+// console.log(cars);
+// console.log(cars2);
+// console.log(cars3);
+// console.log(typeof(cars+cars2)); //array string e döndü
+// console.log(cars+cars2);
+
+// const points = [40, 100, 1, 5, 25, 10]
+// console.log(points.sort()); // sıralama string mantıkla ilerliyor
+// console.log(points.sort(function(a, b){return a - b})); //sayıları düzgün sıralamaya yarıyor
+
+// const cars = ['Audi', 'Opel', 'Fiat']
+// cars.push('Renault')
+// console.log(cars);
+// cars.pop() //parametre almaz hep sonuncuyu siler
+// console.log(cars);
+
+// const cars = ['Audi', 'Opel', 'Fiat']
+// console.log(cars.shift());
+// console.log(cars);
+// const added = cars.unshift('VW')
+// console.log(cars);
+// console.log('Geri dönen',added);
+
+// const cars = ['Audi', 'Opel', 'Fiat']
+// console.log(cars);
+// cars[6] = 'vW'
+// console.log(cars);
+// console.log(cars[3]);
+// cars = ['vW']
+// console.log(cars); //method larla içeriğini değiştirirsin ama atama olarak olmaz
