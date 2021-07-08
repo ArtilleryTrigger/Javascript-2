5027801d9bb652d8c895bde86fe5c9773e2da09b
ArtilleryTrigger <artillerytrigger@gmail.com>
2021-07-08 21:27:06 +0300

    init

---
+++ Dom/dom.js
@@ -0,0 +1,36 @@
+//document.getElementBy...
+/*const myDom = document.getElementById("dom")
+console.log(myDom);*/
+/*const instructor = document.getElementById("info");
+console.log(instructor);*/
+/*const ins = document.getElementById("instructor");
+console.log(ins);*/
+
+//Classname
+/*const withClassName = document.getElementsByClassName('class-info');
+console.log(withClassName[0]);
+console.log(withClassName);*/
+
+//Tagname
+/*const cw = document.getElementsByTagName('h3')
+console.log(cw);*/
+
+//documnet.querySelector
+/*console.log(document.querySelectorAll(".class-info"));
+console.log(document.querySelector("#i̇nstructor"));
+console.log(document.querySelectorAll("h3")); //All yapmazsak gördüğü ilk tag i alır
+console.log(document.querySelectorAll(".class-info h3"));*/
+
+//Manipulation
+/*const brands = document.querySelector("#brands");
+console.log(brands);
+//brands.innerHTML += "<li>NioyaTech</li>"
+brands.innerHTML = brands.innerHTML + "<li class='brand'>NioyaTech</li>";*/
+/*const classBrands = document.querySelectorAll('.brand');
+console.log(classBrands[3].innerText);*/
+
+/*const copyRight = document.querySelector('footer p');
+//console.log(copyRight);
+copyRight.innerHTML = '<h1>&copy; 2021 clarusway.com</h1>';
+//copyRight.innerText = '<h1>&copy; 2021 clarusway.com</h1>';*/
+
